9e9c504db493032f5a01f4b68c7d8cbfaf6cc70b
rahulkundu1209 <121861377+rahulkundu1209@users.noreply.github.com>
2023-06-22 10:24:57 +0530

    Updated all the components and App.js is updated with the components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
-import Budget from './components/Budget';
+import Budget from './components/Budget.js';
 
 // Add code to import the other components here under
+import Remaining from './components/Remaining.js';
+import ExpenseTotal from './components/ExpenseTotal.js';
+import ExpenseList from './components/ExpenseTotal.js';
+import ExpenseItem from './components/ExpenseItem.js';
+import AllocationForm from './components/AllocationForm.js';
 
 
 import { AppProvider } from './context/AppContext';
@@ -15,19 +20,11 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                        <div className='col-sm'>
+                            <Budget/>    
+                        </div>       
 
-                        {/* Add Remaining component here under */}        
-
-                        {/* Add ExpenseTotal component here under */}        
-                       
-                        {/* Add ExpenseList component here under */}         
-
-                        {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
-
-                </div>
+                    </div>
             </div>
         </AppProvider>
     );
